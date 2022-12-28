97118505cf334425eecb5192e50bff90f5851826
Jason Wicker <jlwicker@gmail.com>
2022-12-28 13:36:35 -0700

    create user on invite

--- packages/app/pages/api/member/invite.ts
+++ packages/app/pages/api/member/invite.ts
@@ -2,6 +2,7 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import { ApiResponse, InviteLink } from '@/lib/models'
 import { withMember, withMethods } from 'lib/utils/server'
 import { sendNotificationEmail } from 'lib/services/sendgrid/server'
+import { createUser } from '../../../lib/services/directus/server'
 
 async function Invite(req: NextApiRequest, res: NextApiResponse<ApiResponse>) {
   try {
@@ -11,7 +12,6 @@ async function Invite(req: NextApiRequest, res: NextApiResponse<ApiResponse>) {
     if (!member) return res.status(401).end()
 
     const { email, link } = req.body as InviteLink
-
     await sendNotificationEmail(
       email,
       `${member.first_name} ${member.last_name} has invited you to join our community!`,
@@ -19,6 +19,15 @@ async function Invite(req: NextApiRequest, res: NextApiResponse<ApiResponse>) {
       `Accept Invitation`,
       link
     )
+    await createUser({
+      email,
+      user_type: 'subscriber',
+      status: 'new',
+      vouched_by: member.id,
+      notes: `Invited by ${member.first_name} ${member.last_name}`,
+      application_status: 'apply',
+      in_sendgrid: true,
+    })
 
     res.status(200).end()
   } catch (e: any) {
