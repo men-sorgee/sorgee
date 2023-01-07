11fdcd68c18422ab600c7db2d7af2d5d2a00d0d9
Jason Wicker <jlwicker@gmail.com>
2023-01-07 02:43:15 -0700

    added twilio defaults

--- packages/app/lib/services/twilio/server.ts
+++ packages/app/lib/services/twilio/server.ts
@@ -1,10 +1,16 @@
 import twilio from 'twilio'
 
+const config = {
+  twilioSid: process.env.TWILIO_SID || 'AC8581eae8d3c89559f3eaf62f6e206d2b',
+  twilioToken: process.env.TWILIO_TOKEN,
+  twilioNumber: process.env.TWILIO_NUMBER || '+17207249426',
+}
+
 export async function sendNotification(phone: string, message: string) {
-  const client = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN)
+  const client = twilio(config.twilioSid, config.twilioToken)
   const result = await client.messages.create({
     body: message,
-    from: process.env.TWILIO_NUMBER,
+    from: config.twilioNumber,
     to: phone,
   })
   return result
