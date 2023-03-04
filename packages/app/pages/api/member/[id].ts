e5790f6c11553fc3f6492ab3c8609e63e656ea04
Jason Wicker <jlwicker@gmail.com>
2023-03-04 16:00:27 -0700

    fixed the seeing non-members through the api

--- packages/app/pages/api/member/[id].ts
+++ packages/app/pages/api/member/[id].ts
@@ -28,15 +28,6 @@ export default async function getMemberDetails(
       if (!user) {
         return res.status(404).json(ApiResponse(null, 'Not found'))
       }
-      const user_level = MemberLevel[user.user_type]
-
-      if (viewer_level < MemberLevel.big_brother && user_level < MemberLevel.brother) {
-        return res.status(401).json(ApiResponse(null, 'Unauthorized'))
-      }
-
-      if (viewer_level < MemberLevel.staff && user.show_profile == false) {
-        return res.status(401).json(ApiResponse(null, 'Unauthorized'))
-      }
     }
 
     switch (method) {
