ebd89c012912f9fb063634a5c448cf0040e47948
Jason Wicker <jlwicker@gmail.com>
2023-03-21 02:47:06 -0600

    added cookie policy

---
+++ packages/app/pages/cookies.tsx
@@ -0,0 +1,25 @@
+import Script from 'next/script'
+import { useMeta } from 'hooks/use-meta'
+import { useEffect } from 'react'
+
+export default function Privacy() {
+  const { setMeta } = useMeta()
+  useEffect(() => {
+    setMeta('Cookie Policy')
+  })
+  return (
+    <>
+      <div
+        name={'termly-embed'}
+        data-id="ae99ba16-765a-45c0-801d-d12de3a5ae93"
+        data-type="iframe"
+      ></div>
+      <Script
+        strategy="afterInteractive"
+        id="termly-jssdk"
+        src="https://app.termly.io/embed-policy.min.js"
+        type="text/javascript"
+      />
+    </>
+  )
+}
