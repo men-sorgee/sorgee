a4a820a299340a9694cb5e123b973ff0f7e056ce
Jason Wicker <jlwicker@gmail.com>
2023-03-31 23:31:21 -0600

    page revalidate

--- packages/app/pages/api/revalidate/[...slug].ts
+++ packages/app/pages/api/revalidate/[...slug].ts
@@ -4,18 +4,20 @@ import { withMethods } from 'lib/utils/server'
 
 export default async function Revalidate(req: NextApiRequest, res: NextApiResponse<ApiResponse>) {
   try {
-    if (!withMethods(req, ['POST', 'GET'])) return
+    withMethods(req, ['POST', 'GET'])
 
     if (req.headers.authorization !== process.env.ADMIN_TOKEN)
       return res.status(401).json(ApiResponse(null, 'Unauthorized'))
 
-    let slug = (req.query.slug || req.body.slug) as string
-    if (!slug) return res.status(400).json(ApiResponse(null, 'Missing slug'))
-
-    if (slug.includes('index')) slug = ''
-
-    // This should be the actual path not a rewritten path
-    await res.revalidate(slug?.at(0) == '/' ? slug : '/' + slug)
+    let s = req.query.slug || (req.body.slug as string[])
+    if (!s) return res.status(400).json(ApiResponse(null, 'Missing slug'))
+    let slug = Array.isArray(s) ? s[s.length - 1] : s
+    if (slug.includes('index')) {
+      await res.revalidate('/')
+    } else {
+      // This should be the actual path not a rewritten path
+      await res.revalidate(slug)
+    }
     return res.json(ApiResponse({ revalidated: true }))
   } catch (err) {
     // If there was an error, Next.js will continue
