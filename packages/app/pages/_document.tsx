1bde5dfa7765583343462ceb67462b0c04b59341
Jason Wicker <jlwicker@gmail.com>
2023-04-27 14:59:29 -0600

    nit

--- packages/app/pages/_document.tsx
+++ packages/app/pages/_document.tsx
@@ -44,7 +44,12 @@ class MyDocument extends Document {
             data-auto-block="on"
             data-website-uuid="8fbb3f3c-9fc6-4256-ad1f-7c061dabb965"
           />
-          <Script async src="https://api.buglog.io/website/80W58ZYWE9/code" strategy="lazyOnload" />
+          <Script
+            async
+            id="bug-log"
+            src="https://api.buglog.io/website/80W58ZYWE9/code"
+            strategy="lazyOnload"
+          />
         </body>
       </Html>
     )
