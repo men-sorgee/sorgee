c1005c3437c0e88a2c7ee05c7b41f30336b8bb61
Jason Wicker <jlwicker@gmail.com>
2023-05-19 02:23:03 -0600

    nit

--- packages/app/hooks/use-messages.tsx
+++ packages/app/hooks/use-messages.tsx
@@ -99,6 +99,7 @@ export function MessagesProvider({ children }: { children: ReactNode | ReactNode
           picture: `/api/asset/${user.picture}?w=100&h=100&fit=crop`,
         },
       })
+      // @ts-ignore
       setConversations(convos.sort((a, b) => b.lastMessage.timestamp - a.lastMessage.timestamp))
       totalNewMessages.push(...newMessages)
     })
