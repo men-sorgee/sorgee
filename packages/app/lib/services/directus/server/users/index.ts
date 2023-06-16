4f3be91e93dcbbc7bb4c301964c3e8ae3d216135
Jason Wicker <jlwicker@gmail.com>
2023-06-15 22:06:25 -0600

    ignore

--- packages/app/lib/services/directus/server/users/index.ts
+++ packages/app/lib/services/directus/server/users/index.ts
@@ -1,3 +1,5 @@
+//@ts-ignore-file
+
 import {
   Applicant,
   applicantFields,
@@ -27,7 +29,7 @@ export async function createUser(member: Partial<User>): Promise<User> {
 
 export async function updateUser<T = User>(id: string, userData: Partial<T>) {
   const adminClient = await getAdminClient()
-  //@ts-ignore
+
   const user = await adminClient.items('users').updateOne(id, userData)
   return user as T
 }
