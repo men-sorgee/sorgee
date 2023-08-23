f30e9f7aac56b6f46aa6545fe08a4495d2ace5b9
Jason Wicker <jlwicker@gmail.com>
2023-08-23 17:07:08 -0600

    Added login redirects to member-profile page

--- packages/app/src/pages/member/[id].tsx
+++ packages/app/src/pages/member/[id].tsx
@@ -1,5 +1,7 @@
 import { MemberSpotlight, Page } from "components";
-import { useFields, useMember } from "hooks";
+import { useFields, useMember, useUser } from "hooks";
+
+import { MemberLevel } from "../../lib/models";
 
 export function getServerSideProps({ params }) {
   return {
@@ -10,11 +12,17 @@ export function getServerSideProps({ params }) {
 }
 
 export default function MemberPage({ id }: { id: string }) {
+  const { member, loading: memberLoading } = useUser({
+    minLevel: MemberLevel.pledge,
+    redirectsEnabled: true
+  })
   const { name, loading } = useMember(id)
   const { fields, loading: fieldsLoading } = useFields('users')
 
+  if (!member) return null
+
   return (
-    <Page hideHeader title={name} pt={10} loading={loading || fieldsLoading}>
+    <Page hideHeader title={name} pt={10} loading={loading || fieldsLoading || memberLoading}>
       <MemberSpotlight memberId={id} fields={fields} full size="2xl" />
     </Page>
   )
