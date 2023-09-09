5089774ede0bb046f3a7326d1f4988f82d173ef4
Jason Wicker <jlwicker@gmail.com>
2023-09-08 21:46:54 -0600

    nit

--- packages/app/src/lib/utils/index.ts
+++ packages/app/src/lib/utils/index.ts
@@ -22,7 +22,7 @@ export const getUTCNow = () => {
 export const gradient = (color: string, value: number = 400, step: number = 100) => `linear(to-b, ${color}.${value}, ${color}.${value + step}, ${color}.${value + step})`
 
 export const uuidv4 = () => {
-  return 'xxxxxxxx-xxxx-4xxx'.replace(/[xy]/g, function (c) {
+  return 'xxxxxxxx-xxxx-4xxx-xxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
     var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8)
     return v.toString(16)
   });
